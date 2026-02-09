2f75404aedce26b23507e25fb1baec8ab1b6e243
Parsibor <parsibor@yandex.ru>
2026-02-09 20:31:13 +0300

    create and add initial code

---
+++ src/widget.py
@@ -0,0 +1,10 @@
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card_number: str) -> str:
+    pass
+
+
+if __name__ == "__main__":
+    card = input("Введите карту: ")
+    print(mask_account_card(card))
