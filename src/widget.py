8e34bf1575a02aba03b20a487097056f9c7c8954
Parsibor <parsibor@yandex.ru>
2026-02-09 21:29:17 +0300

    widget.py - intermediate ver 3

--- src/widget.py
+++ src/widget.py
@@ -3,11 +3,18 @@ from masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(account_card: str) -> str:
     if account_card[:4].lower() == "счет":
-        return "Счет " + get_mask_account(account_card[-20:])
+        mask_account = "Счет " + get_mask_account(account_card[-20:])
+        return mask_account
     else:
-        return account_card[0:-16] + " " + get_mask_card_number(account_card[-16:])
+        mask_card = account_card[0:-16] + " " + get_mask_card_number(account_card[-16:])
+        return mask_card
 
+
+def get_date(date: str) -> str:
+    new_date = date[8:10]+"."+date[5:7]+"."+date[:4]
+    return new_date
 
 if __name__ == "__main__":
     account_card = input("Введите карту или счет: ")
     print(mask_account_card(account_card))
+    print(get_date("2024-03-11T02:26:18.671407"))
