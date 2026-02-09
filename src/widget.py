1a35dc011f0fa512a736d07e38a1b70a54d87889
Parsibor <parsibor@yandex.ru>
2026-02-09 21:06:27 +0300

    widget.py - intermediare ver 2

--- src/widget.py
+++ src/widget.py
@@ -4,14 +4,10 @@ from masks import get_mask_card_number, get_mask_account
 def mask_account_card(account_card: str) -> str:
     if account_card[:4].lower() == "счет":
         return "Счет " + get_mask_account(account_card[-20:])
-        # return account_card[-16:]
     else:
         return account_card[0:-16] + " " + get_mask_card_number(account_card[-16:])
-        return "Hello!"
 
 
 if __name__ == "__main__":
     account_card = input("Введите карту или счет: ")
     print(mask_account_card(account_card))
-    print(account_card[0:-16])
-
