b2d731088b663c1a8b45336a1badcdb69ca617ad
Parsibor <parsibor@yandex.ru>
2026-02-18 21:24:04 +0300

    base test filter_by_state()

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,26 @@
-def filter_by_state():
+from typing import Any
+
+test_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(list_dict: list, state: str = 'EXECUTED') -> list:
     """Функция, которая принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует
     указанному значению."""
-    pass
+    new_list: list[Any] = []
+
+    for dict in list_dict:
+        # if state == 'EXECUTED':
+        print(dict["state"])
+        if dict["state"] == state:
+            new_list.append(dict)
+
+    return new_list
 
 
 def sort_by_date():
@@ -11,6 +29,6 @@ def sort_by_date():
     pass
 
 
-if __name__ == '__main__':
-    # filter_by_state()
+if __name__ == "__main__":
+    print(filter_by_state(test_list))
     # sort_by_date()
