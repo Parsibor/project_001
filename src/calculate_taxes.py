e2561916eb71504fc4eab0b81ae1b8552deec2e2
Parsibor <parsibor@yandex.ru>
2026-03-05 23:11:17 +0300

    add calculate_tax()

--- src/calculate_taxes.py
+++ src/calculate_taxes.py
@@ -16,9 +16,29 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+#--------------------------------------------------------------------------------------
+
+
+def calculate_tax(price: float, tax_rate: float, discount: float = 0, rounder: float = 2) -> float:
+
+    # if type(price) or type(tax_rate) is not float:
+    #     raise TypeError('Аргумент не является числом')
+
+    if price < 0:
+        raise ValueError('Неверная цена')
+
+    if tax_rate < 0 or tax_rate >= 100:
+        raise ValueError('Неверный налоговый процент')
 
 
 
 
+
+    return round((price + (price * tax_rate / 100)) * (1 - discount / 100), rounder)
+
+
+
+#--------------------------------------------------------------------------------------
 if __name__ == '__main__':
     print(calculate_taxes([230,45,32], 5))
+    print(calculate_tax(113, 13, 12))
--- tests/test_calculate_taxes.py
+++ tests/test_calculate_taxes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.calculate_taxes import calculate_taxes
+from src.calculate_taxes import calculate_taxes, calculate_tax
 
 def test_calculate_taxes_value_under_zero():
     with pytest.raises(ValueError, match='Неверный налоговый процент'):
@@ -13,4 +13,46 @@ def test_calculate_taxes_price_zero():
 def test_calculate_taxes_prices_is_not_list():
     with pytest.raises(TypeError,match='Аргумент не является списком'):
         calculate_taxes(0,0)
+#--------------------------------------------------------------------------------------
 
+def test_calculate_tax_price_under_zero():
+    with pytest.raises(ValueError, match='Неверная цена'):
+        calculate_tax(-1, 1)
+
+def test_calculate_tax_tax_rate_under_limit():
+    with pytest.raises(ValueError, match='Неверный налоговый процент'):
+        calculate_tax(12, -1)
+        calculate_tax(12, 100)
+        calculate_tax(12, 101)
+
+def test_calculate_tax_discount():
+    pass
+
+
+
+
+
+
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_1():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax(12, 'd')
+#
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_2():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax('d', 12)
+#
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_3():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax([12], 5)
+#
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_4():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax((12,23,'ds'), 5)
+#
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_5():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax(12, ['d', 14, True])
+#
+# def test_calculate_tax_prices_and_tax_rate_is_not_float_6():
+#     with pytest.raises(TypeError, match='Аргумент не является числом'):
+#         calculate_tax(12, (12,'d,True'))
