f1cc36eb82b2da3ce0a496983b5da523cde37059
Parsibor <parsibor@yandex.ru>
2026-03-09 12:33:12 +0300

    full homework 10.2

---
+++ tests/test_masks.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize(
+    "card_number, mask_expected",
+    [
+        ("1", "Введено неверное количество символов номера карты. Должно быть 16."),
+        ("", "Введено неверное количество символов номера карты. Должно быть 16."),
+        ("11112222333344445", "Введено неверное количество символов номера карты. Должно быть 16."),
+        ("1111111111111111", "1111 11** **** 1111"),
+    ],
+)
+def test_get_mask_card_number(card_number, mask_expected):
+    assert get_mask_card_number(card_number) == mask_expected
+
+
+@pytest.mark.parametrize("wrong_type", [(1), ([1]), ((1)), (True)])
+def test_get_mask_card_number_type_error(wrong_type):
+    with pytest.raises(TypeError):
+        get_mask_card_number(wrong_type)
+
+
+@pytest.mark.parametrize(
+    "account_number, account_mask_expected",
+    [("6363636363636363", "**6363"), ("123", "**123"), ("111122223333444455556666", "**6666")],
+)
+def test_get_mask_account(account_number, account_mask_expected):
+    assert get_mask_account(account_number) == account_mask_expected
---
+++ tests/test_widget.py
@@ -0,0 +1,34 @@
+import pytest
+
+from src.widget import mask_account_card, get_date
+
+
+@pytest.mark.parametrize('arg, expected', [('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+                                           ('Maestro 7000792289606361', 'Maestro 7000 79** **** 6361'),
+                                           ('Счет 73654108430135874305', 'Счет **4305')])
+
+def test_mask_account_card(arg, expected):
+    assert mask_account_card(arg) == expected
+
+
+@pytest.mark.parametrize("wrong_type", [(1), ([1]), ((1)), (True)])
+def test_mask_account_card_type_error(wrong_type):
+    with pytest.raises(TypeError):
+        mask_account_card(wrong_type)
+
+@pytest.mark.parametrize("date, expected", [('2024-03-11T02:26:18.671407', '11.03.2024'),
+                                            ('2024-03-11T02:26', '11.03.2024'),
+                                            ('2024-03-1', 'Передан неверные формат или длина даты'),
+                                            ('', 'Передан неверные формат или длина даты'),
+                                            ('12/12/2023', '12.12.2023'),
+                                            ('ssssssssssssssssssssss', 'В поле ДАТА передано что-то не то!')
+                                            ])
+
+def test_get_date1(date, expected):
+    assert get_date(date) == expected
+
+
+@pytest.mark.parametrize("wrong_type", [(1), ([1]), ((1)), (True)])
+def test_get_date_type_error(wrong_type):
+    with pytest.raises(TypeError):
+        get_date(wrong_type)
